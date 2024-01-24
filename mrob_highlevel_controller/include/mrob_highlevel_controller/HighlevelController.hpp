3a4f7d08d41b8266cbdd659e7ab1d49c4d7e15d1
PierreDesbruns <pierre.desbruns@gmail.com>
2024-01-24 12:10:32 +0100

    TP2 finished with vscode config directory

--- mrob_highlevel_controller/include/mrob_highlevel_controller/HighlevelController.hpp
+++ mrob_highlevel_controller/include/mrob_highlevel_controller/HighlevelController.hpp
@@ -3,6 +3,9 @@
 // ROS
 #include <ros/ros.h>
 
+// LaserScan
+#include "sensor_msgs/LaserScan.h"
+
 namespace mrob_highlevel_controller {
 
 /*!
@@ -22,9 +25,19 @@ class HighlevelController
    */
   virtual ~HighlevelController();
 
+  /*!
+   * Callback function for /scan topic.
+   * Prints in ROS_INFO the smallest distance measurement from vector ranges.
+   * @param scanMsg message from /scan topic
+   */
+  void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scanMsg);
+
  private:
   //! ROS node handle.
   ros::NodeHandle& nodeHandle_;
+
+  //! ROS subscriber to /scan topic
+  ros::Subscriber scanSub_;
 };
 
 } /* namespace */
--- mrob_highlevel_controller/src/HighlevelController.cpp
+++ mrob_highlevel_controller/src/HighlevelController.cpp
@@ -8,10 +8,16 @@ namespace mrob_highlevel_controller {
 HighlevelController::HighlevelController(ros::NodeHandle& nodeHandle)
     : nodeHandle_(nodeHandle)
 {
+    scanSub_ = nodeHandle.subscribe("/scan", 1000, &HighlevelController::scanCallback, this);
 }
 
 HighlevelController::~HighlevelController()
 {
+}
+
+void HighlevelController::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scanMsg)
+{
+    ROS_INFO("Smallest distance in scan ranges: [%f]", *min_element(scanMsg->ranges.begin(), scanMsg->ranges.end()));
 }
 
 } /* namespace */
